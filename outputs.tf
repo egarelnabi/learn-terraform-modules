752d0ac419b2058eba9f809d36c127e42f3d067e
egarelnabi <egarelnabi@users.noreply.github.com>
2020-09-15 17:06:36 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
   value       = module.ec2_instances.public_ip
 }
-
+/*
 output "website_bucket_arn" {
   description = "ARN of the bucket"
   value       = module.website_s3_bucket.arn
@@ -23,4 +23,4 @@ output "website_bucket_name" {
 output "website_endpoint" {
   description = "Domain name of the bucket"
   value       = module.website_s3_bucket.website_endpoint
-}
+}*/
