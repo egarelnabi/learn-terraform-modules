83e794579a912e0a73629baa6d367a37f1cbdcfe
egarelnabi <egarelnabi@users.noreply.github.com>
2020-09-15 17:39:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source  = "github.com/egarelnabi/terraform-aws-vpc"
+  source  = "git::https://github.com/egarelnabi/terraform-aws-vpc.git?ref=v2.21.0"
   //version = "2.21.0"
 
   name = var.vpc_name
